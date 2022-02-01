b8d217436480f49327fbc6194c22df312612ddca
atakan yontar <atakanyontar@gmail.com>
2022-02-02 01:46:48 +0300

    moved files to necessary folders to organize

--- AreaofCircle.js
+++
@@ -1,6 +0,0 @@
-function areaOfCircle(radius){
-	let area = Math.PI * (radius *1)**2
-	console.log("PI x r^2 : "+area)
-}
-
-areaOfCircle(process.argv[2])
--- Javascript-study/08-prompt.js
+++
@@ -1,3 +0,0 @@
-let fullName = prompt("Lutfen adinizi giriniz:" ,"");
-let greeting = document.querySelector("#greeting");
-greeting.innerHTML = `${greeting.innerHTML} <small style="color:red">${fullName}</small>`
